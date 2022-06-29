7b4e6e6ba0d9b7a8e9953090f017b8a5d7edd46f
github username <um_2003_8@hotmail.com>
2022-06-29 11:17:56 -0400

    create a method

--- java-classes-methods-objects/src/App.java
+++ java-classes-methods-objects/src/App.java
@@ -1,6 +1,13 @@
 
+class Methods{
+	void run() {
+		System.out.println("Running..");
+	}
+}
+
 public class App {
 	public static void main(String[] args) {
-
+		Methods name = new Methods();
+		name.run();
 	}
 }
